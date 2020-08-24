450aef4db2f5095e7b5d8b33b5c48f8f0d61788e
Nekuskus <61391965+Nekuskus@users.noreply.github.com>
2020-08-24 20:07:48 +0200

    Added base layout for the Chat window

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,20 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Chat_TextBox = new System.Windows.Forms.RichTextBox();
+            this.Input_TextBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Names_Label = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // Chat_TextBox
+            // 
+            this.Chat_TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Chat_TextBox.Location = new System.Drawing.Point(13, 45);
+            this.Chat_TextBox.Name = "Chat_TextBox";
+            this.Chat_TextBox.Size = new System.Drawing.Size(344, 338);
+            this.Chat_TextBox.TabIndex = 0;
+            this.Chat_TextBox.Text = "";
+            // 
+            // Input_TextBox
+            // 
+            this.Input_TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Input_TextBox.Location = new System.Drawing.Point(13, 400);
+            this.Input_TextBox.Name = "Input_TextBox";
+            this.Input_TextBox.Size = new System.Drawing.Size(290, 38);
+            this.Input_TextBox.TabIndex = 1;
+            this.Input_TextBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(310, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(47, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Names_Label
+            // 
+            this.Names_Label.AutoSize = true;
+            this.Names_Label.Location = new System.Drawing.Point(13, 13);
+            this.Names_Label.Name = "Names_Label";
+            this.Names_Label.Size = new System.Drawing.Size(124, 26);
+            this.Names_Label.TabIndex = 3;
+            this.Names_Label.Text = "You: Nick_1 (Color_1)\r\nEnemy: Nick_2 (Color_2)";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(369, 450);
+            this.Controls.Add(this.Names_Label);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Input_TextBox);
+            this.Controls.Add(this.Chat_TextBox);
             this.Name = "Form1";
             this.Text = "Czat";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox Chat_TextBox;
+        private System.Windows.Forms.RichTextBox Input_TextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Names_Label;
     }
 }
 
